86ccb785449a19083a9cca4ae14048e387be8f90
makemake <vukasin@gostovic.me>
2024-10-16 15:14:57 +0200

    add leaf fanout const

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,15 @@ use clap::Parser;
 
 use anyhow::Result;
 
+pub const LEAF_FANOUT: usize = 1024;
+
 #[tokio::main]
 async fn main() -> Result<()> {
 	// Parse CLI args
 	let config = config::ExecutorConfig::parse();
 
 	// Open the executor Db
-	let db: Db<1024> = open_sled!(config);
+	let db: Db<LEAF_FANOUT> = open_sled!(config);
 
 	let memory_db: MemoryDb<64> = init_mem_db!(config);
 
